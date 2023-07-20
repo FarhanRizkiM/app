e7c138642e5144cf3df0c342a9258f97003d1c8f
Farhan Rizki <farhanrizki101010@gmail.com>
2023-07-20 11:22:55 +0700

    edit

--- akademik/presensi/js/controller/croot.js
+++ akademik/presensi/js/controller/croot.js
@@ -1,28 +1,23 @@
 import { addInner } from "https://jscroot.github.io/element/croot.js";
 import { get } from "https://jscroot.github.io/api/croot.js"; 
-import { getRandomColorName } from "./randomcol.js";
-import {rowtabel, svg ,smt} from "../template/tabel.js";
-import { URLPresensi } from "../config/url.js";
-get(URLPresensi, tabelContent);
+import { rowtabel } from "../template/tabel.js";
+import { URLPrs } from "../config/url.js";
+get(URLPrs, tabelContent);
 
 
 function tabelContent(result){
-    userTable(result);
+    prsTable(result);
 }
 
-function userTable(jsonParse){
+function prsTable(jsonParse){
     let row = '';
     jsonParse.forEach((element) => {
-    let svgicon = svg.replace("#WARNA#", getRandomColorName());
-    let samat = smt.replace("#col#", getRandomColorName());
-    console.log(jabat);
     row = rowtabel.replace("#nama#", element.nama).
-        replace("#npm#", element.status).
-        replace("#smt#", samat).
+        replace("#npm#", element.npm).
         replace("#semester#", element.semester).
-        replace("#kelas#", element.gaji).
-        replace("#prodi_kampus#", element.prodi_kampus).
-        replace("#SVG#", svgicon);
-    addInner("demo",row);
+        replace("#kelas#", element.kelas).
+        replace("#prodi_kampus#", element.prodi_kampus);
+        console.log(element._id);
+        addInner("demo",row);
     });
 }
--- akademik/presensi/js/template/tabel.js
+++ akademik/presensi/js/template/tabel.js
@@ -3,7 +3,7 @@ export let rowtabel = `<tr>
   <div class="flex items-center">
     <div class="flex-shrink-0 h-10 w-10">
       <img class="h-10 w-10 rounded-full"
-        
+      #SVG#
     </div>
     <div class="ml-4">
       <div class="text-sm leading-5 font-medium text-gray-900">#nama#</div>
@@ -15,14 +15,10 @@ export let rowtabel = `<tr>
   <div class="text-sm leading-5 text-gray-900">#npm#</div>
 </td>
 <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-<div class="text-sm leading-5 text-gray-900">#smt#</div>
+<div class="text-sm leading-5 text-gray-900">#semester#</div>
 </td>
 <td
   class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 text-gray-500">
   #kelas#</td>
 </tr>`
 
-export let svg = `<div class="flex items-center justify-center w-10 h-10 text-base font-medium text-yellow-600 bg-#WARNA#-200 rounded-md">PS</div>`
-
-
-export let smt = `<div class="text-sm leading-5 font-medium text-gray-900">#semester#</div>`
