6b17b8594b589a5c3913d27714ea2966d8a40ea2
Farhan Rizki <farhanrizki101010@gmail.com>
2023-07-23 18:15:07 +0700

    add file

--- akademik/presensi/js/controller/croot.js
+++ akademik/presensi/js/controller/croot.js
@@ -1,23 +1,28 @@
 import { addInner } from "https://jscroot.github.io/element/croot.js";
 import { get } from "https://jscroot.github.io/api/croot.js"; 
-import { rowtabel } from "../template/tabel.js";
-import { URLPrs } from "../config/url.js";
-get(URLPrs, tabelContent);
+import { getRandomColorName } from "./randomcol.js";
+import { rowtabel, svg, smt } from "../template/tabel.js";
+import { URLPresensi } from "../config/url.js";
+get(URLPresensi, tabelContent);
 
 
 function tabelContent(result){
-    prsTable(result);
+    userTable(result);
 }
 
-function prsTable(jsonParse){
+function userTable(jsonParse){
     let row = '';
     jsonParse.forEach((element) => {
-    row = rowtabel.replace("#nama#", element.nama).
-        replace("#npm#", element.npm).
+    let svgicon = svg.replace("#WARNA#", getRandomColorName());
+    let smtcol = smt.replace("#col#", getRandomColorName());
+    console.log(smtcol);
+    row = rowtabel.replace("#Nama#", element.nama).
+        replace("#Npm#", element.npm).
+        replace("#smt#", smtcol).
         replace("#semester#", element.semester).
         replace("#kelas#", element.kelas).
-        replace("#prodi_kampus#", element.prodi_kampus);
-        console.log(element._id);
-        addInner("demo",row);
+        replace("#prodi_kampus#", element.prodi_kampus).
+        replace("#SVG#", svgicon);
+    addInner("demo",row);
     });
 }
